328dad8dd32008f529638ca67178a2586e0cc6c7
Shashank Manu Rao <shashank@Shashanks-MacBook-Pro.local>
2026-05-27 13:40:39 +0530

    model_loader added

--- src/AI_Trip_Planner/utils/config_loader.py
+++ src/AI_Trip_Planner/utils/config_loader.py
@@ -4,5 +4,5 @@ import os
 def load_config(config_path: str = "config/config.yaml") -> dict:
     with open(config_path, "r") as file:
         config = yaml.safe_load(file)
-        # print(config)
     return config
+
---
+++ src/AI_Trip_Planner/utils/model_loader.py
@@ -0,0 +1,34 @@
+import os
+from dotenv import load_dotenv
+from typing import Literal, Optional, Any
+from pydantic import BaseModel, Field
+from utils.config_loader import load_config
+from langchain_groq import ChatGroq
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+class ConfigLoader:
+    def __init__(self):
+        self.config = load_config()
+        print(f"Config loaded successfully.")
+
+    def __getitem__(self,key):
+        return self.config[key]
+
+class ModelLoader(BaseModel):
+    model_provider: Literal["google", "groq"] = "groq"
+    config: Optional[ConfigLoader] = Field(default=None, exclude=True)
+
+    def model_post_init(self, __context: Any) -> None:
+        self.config = ConfigLoader()
+
+    def load_llm(self):
+        print(f"LLM loading from provider: {self.model_provider}")
+        if self.model_provider == "google":
+            google_api_key = os.getenv("GOOGLE_API_KEY")
+            model_name = self.config["llm"]["google"]["model_name"]
+            llm = ChatGoogleGenerativeAI(model=model_name, google_api_key=google_api_key)
+        elif self.model_provider == "groq":
+            groq_api_key = os.getenv("GROQ_API_KEY")
+            model_name = self.config["llm"]["groq"]["model_name"]
+            llm = ChatGroq(model=model_name, api_key=groq_api_key)
+        return llm
